37588bb312fad07b6557cc811d5a277db5718904
Yonatan Hershko <yonher8@gmail.com>
2024-08-21 14:03:31 +0300

    Basic Starter

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style >
+
+</style>
